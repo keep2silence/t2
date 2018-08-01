3605e18d7e94ccfa6b0253882b470e252e712b0a
duht <duht@aliyun.com>
2018-08-01 18:34:57 +0800

    dev

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# t2
+# t2 ，部分代码来自于https://github.com/taurusai/kungfu
--- md_engine_test.cpp
+++ md_engine_test.cpp
@@ -1,10 +1,26 @@
 #include "ctp_md_engine.h"
 #include <unistd.h>
 
+
+class quot_consumer : public md_event_listener
+{
+public:
+	void handle_quot (quot_t* quot_ptr);
+};
+
+void quot_consumer::handle_quot (quot_t* quot_ptr)
+{
+	pr_debug ("bid_price: %f, ask_price: %f, bid_qty: %d, ask_qty: %d\n",
+		quot_ptr->bid_price, quot_ptr->ask_price, quot_ptr->bid_qty, quot_ptr->ask_qty);
+}
+
 int
 main ()
 {
 	i_md_engine* engine_ptr = new ctp_md_engine;
+	auto quot_consumer_ptr = new quot_consumer;
+	quot_consumer_ptr->listener_name = std::string ("quot_consumer");
+	engine_ptr->register_md_event_listener (quot_consumer_ptr);
 	std::vector<std::string> contract_vec;
 	contract_vec.push_back (std::string ("rb1901"));
 	engine_ptr->init (std::string ("9999"), std::string ("032862"), 
